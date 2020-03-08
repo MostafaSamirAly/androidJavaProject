f7465425502c306a59892e4b2e5eb57d3261f310
Manal_Taha <336107lkhg>
2020-03-08 06:59:43 +0200

    pass user data to home fragment

--- app/src/main/java/com/example/mishwary/ui/login/LoginContract.java
+++ app/src/main/java/com/example/mishwary/ui/login/LoginContract.java
@@ -1,11 +1,14 @@
 package com.example.mishwary.ui.login;
 
+import com.example.mishwary.Models.User;
+
 public interface LoginContract {
     public interface LoginView{
-        void goToHome();
+        void goToHome(User user);
         void showError();
     }
     public interface LoginPresenter{
         void validateAccount(String email ,String password);
+
     }
 }
--- app/src/main/java/com/example/mishwary/ui/login/LoginPresenter.java
+++ app/src/main/java/com/example/mishwary/ui/login/LoginPresenter.java
@@ -4,8 +4,10 @@ import android.app.DownloadManager;
 
 import androidx.annotation.NonNull;
 
+import com.example.mishwary.Models.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
@@ -25,7 +27,7 @@ public class LoginPresenter implements LoginContract.LoginPresenter {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if(dataSnapshot.getChildrenCount() > 0){
-                    ref.goToHome();
+                    ref.goToHome(null);
                 }else {
                     ref.showError();
                 }
@@ -38,4 +40,6 @@ public class LoginPresenter implements LoginContract.LoginPresenter {
         });
 
     }
+
+
 }
