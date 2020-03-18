62f27a74dca36a8825fa1439a625e1c00e013840
Mostafa <samir.mostafa868@gmail.com>
2020-03-18 12:19:20 +0200

    removing unused lines

--- app/src/main/java/com/example/mishwary/Models/FireBaseCreator.java
+++ app/src/main/java/com/example/mishwary/Models/FireBaseCreator.java
@@ -11,18 +11,3 @@ public class FireBaseCreator {
         return INSTANCE;
     }
 }
-/*
-* package com.journaldev.singleton;
-
-public class BillPughSingleton {
-
-    private BillPughSingleton(){}
-
-    private static class SingletonHelper{
-        private static final BillPughSingleton INSTANCE = new BillPughSingleton();
-    }
-
-    public static BillPughSingleton getInstance(){
-        return SingletonHelper.INSTANCE;
-    }
-}*/
