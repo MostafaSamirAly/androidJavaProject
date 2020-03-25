98c1b58e0dcd8113bc8c50ca7df03521b22fd431
Manal_Taha <336107lkhg>
2020-03-25 20:47:38 +0200

    startbtn in notification

--- app/src/main/java/com/iti/mishwary/ui/addactivity/MyAlertDialog.java
+++ app/src/main/java/com/iti/mishwary/ui/addactivity/MyAlertDialog.java
@@ -1,6 +1,7 @@
 package com.iti.mishwary.ui.addactivity;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.AlertDialog;
@@ -10,11 +11,15 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.Settings;
 
 import androidx.annotation.NonNull;
@@ -22,6 +27,13 @@ import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.iti.mishwary.Models.Trip;
 import com.iti.mishwary.R;
 import com.iti.mishwary.ui.floatingwidget.FloatingWidgetService;
@@ -31,6 +43,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAlertDialog extends Activity {
     public static final String channelID = "channelID";
     public static final String channelName = "Channel Name";
@@ -38,6 +54,12 @@ public class MyAlertDialog extends Activity {
     Ringtone ringtone;
     String userId, tripId;
     Trip mTrip;
+    FusedLocationProviderClient mFusedLocationClient;
+    Geocoder geocoder;
+    List<Address> addresses;
+    String loc;
+    private double longitude;
+    private double latitude;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,7 +68,9 @@ public class MyAlertDialog extends Activity {
         if (alarmUri == null) {
             alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         }
-
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        geocoder = new Geocoder(this);
+        addresses = new ArrayList<>();
         Intent intent = getIntent();
         tripId = intent.getStringExtra("tripId");
         userId = intent.getStringExtra("userId");
@@ -66,7 +90,7 @@ public class MyAlertDialog extends Activity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 removeFromUpcoming();
-                addToHistory();
+
                 //floating icon
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     if (Settings.canDrawOverlays(context)) {
@@ -75,14 +99,18 @@ public class MyAlertDialog extends Activity {
                 }
                 // open google maps with start and destination provided with the path
                 if(mTrip.getStartPoint().equals("At Start Location")){
+                    getLoc();
                     Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?saddr="+"&daddr="+mTrip.getDestination()));
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     context.startActivity(intent);
                 }else{
+
                     Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?saddr="+mTrip.getStartPoint()+"&daddr="+mTrip.getDestination()));
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     context.startActivity(intent);
+                    addToHistory();
                 }
+
                 NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 notificationManager.cancelAll();
                 cancelAlarm();
@@ -186,8 +214,61 @@ public class MyAlertDialog extends Activity {
         databaseReference.child(tripId).setValue(mTrip);
     }
 
+
     private void removeFromUpcoming() {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("upcoming_trip").child(userId);
         databaseReference.child(tripId).removeValue();
     }
+    public void getLoc() {
+        mFusedLocationClient.getLastLocation().addOnCompleteListener(
+                new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocationData();
+                        } else {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                            try {
+                                addresses = geocoder.getFromLocation(
+                                        location.getLatitude(),
+                                        location.getLongitude(),
+                                        1);
+
+                                Address address = addresses.get(0);
+                                loc = address.getAddressLine(0);
+                               mTrip.setStartPoint(loc);
+                               addToHistory();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+
+    }
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest, mLocationCallback,
+                Looper.myLooper()
+        );
+
+    }
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+
+        }
+    };
+
+
 }
